9a8bbcc44a316fa065bd68c0865444d581744350
alex <02009905@corphq.hk.pccw.com>
2024-03-18 09:55:44 +0800

    ch

--- PGPEncryption.java
+++ PGPEncryption.java
@@ -1,6 +1,5 @@
 import java.io.File;
 import java.nio.file.Files;
-import java.nio.file.Path;
 import java.nio.file.attribute.BasicFileAttributeView;
 import java.nio.file.attribute.FileTime;
 import java.security.InvalidKeyException;
@@ -19,48 +18,56 @@ public class PGPEncryption {
         this.encryptFileExtension = encryptFileExtension;
     }
 
-    public void encrypt() throws Exception{
-        File encryptDirectoryfile = new File(encryptDirectory);
-        File[] fileList = encryptDirectoryfile.listFiles();
+    public void encrypt() throws Exception {
+        File encryptDirectoryFile = new File(encryptDirectory);
+        encryptFilesRecursive(encryptDirectoryFile);
+    }
 
-        // Check recipient public key exist
-        Process process = Runtime.getRuntime().exec("gpg --list-key "+recipient);
-        int exitCode = process.waitFor(); 
-        if (exitCode!=0){
-            throw new InvalidKeyException("gpg does not contain recipient public key");
-        }
+    public void encryptFilesRecursive(File directory) throws Exception{
+        File[] fileList = directory.listFiles();
 
         // Encrypt files
-        String[] cmd = {"gpg","-r",recipient,"--batch","--yes","--trust-model","always","-e","-o","",""};
         if (fileList != null){
             for (File file:fileList){
-                BasicFileAttributeView attributes = Files.getFileAttributeView(Path.of(file.toString()), BasicFileAttributeView.class);
-                FileTime creationTime = attributes.readAttributes().creationTime();
-                Instant currentTime = Instant.now();
-                // Check if the file creation time is 15 minutes ago
-                Instant fifteenMinutesAgo = currentTime.minus(15, ChronoUnit.MINUTES);
-                
-                if (endsWithSubstring(file.getAbsolutePath(), "." + encryptFileExtension)) {
-                    if (creationTime.toInstant().isBefore(fifteenMinutesAgo)) {
-                        File gpgfile = new File(file.getAbsolutePath()+".gpg");
-                        if (!gpgfile.exists()){
-                            cmd[cmd.length-2] = file.getAbsolutePath()+".gpg";
-                            cmd[cmd.length-1] = file.getAbsolutePath();
-                            process = Runtime.getRuntime().exec(cmd);
-                            if (process.waitFor()==0){
-                                System.out.println("File encrypted {}." + file.getAbsolutePath());
-                            }else{
-                                throw new Exception("PGPEncryption process returned failed for file: " + file.getAbsolutePath());
-                            }
-                        } else {
-                            System.out.println("This file has already been encrypted: " + file.getAbsolutePath());
-                        }
-                        System.out.println("The file was created 15 minutes ago.");
-                    } else {
-                        System.out.println("The file was not created 15 minutes ago.");
-                    }
+                if (file.isDirectory()) {
+                    encryptFilesRecursive(file);
                 } else {
-                    System.out.println("Invalid file extension: " + file.getAbsolutePath());
+                    // Check recipient public key exist
+                    Process process = Runtime.getRuntime().exec("gpg --list-key "+recipient);
+                    int exitCode = process.waitFor(); 
+                    if (exitCode!=0){
+                        throw new InvalidKeyException("gpg does not contain recipient public key");
+                    }
+
+                    BasicFileAttributeView attributes = Files.getFileAttributeView(file.toPath(), BasicFileAttributeView.class);
+                    FileTime creationTime = attributes.readAttributes().creationTime();
+                    Instant currentTime = Instant.now();
+                    // Check if the file creation time is 15 minutes ago
+                    Instant minsago = currentTime.minus(15, ChronoUnit.MINUTES);
+                    
+                    if (endsWithSubstring(file.getAbsolutePath(), "." + encryptFileExtension)) {
+                        if (creationTime.toInstant().isBefore(minsago)) {
+                            File gpgfile = new File(file.getAbsolutePath()+".gpg");
+                            if (!gpgfile.exists()){
+                                String[] cmd = {"gpg","-r",recipient,"--batch","--yes","--trust-model","always","-e","-o","",""};
+                                cmd[cmd.length-2] = file.getAbsolutePath()+".gpg";
+                                cmd[cmd.length-1] = file.getAbsolutePath();
+                                process = Runtime.getRuntime().exec(cmd);
+                                if (process.waitFor()==0){
+                                    System.out.println("File encrypted {}." + file.getAbsolutePath());
+                                }else{
+                                    throw new Exception("PGPEncryption process returned failed for file: " + file.getAbsolutePath());
+                                }
+                            } else {
+                                System.out.println("This file has already been encrypted: " + file.getAbsolutePath());
+                            }
+                            System.out.println("The file was created 15 minutes ago.");
+                        } else {
+                            System.out.println("The file was not created 15 minutes ago.");
+                        }
+                    } else {
+                        System.out.println("Invalid file extension: " + file.getAbsolutePath());
+                    }
                 }
             }
         } else {
@@ -73,6 +80,12 @@ public class PGPEncryption {
     }
 
     public static void main(String[] args){
+        if (args.length < 2) {
+            System.out.println("Insufficient arguments provided.");
+            System.out.println("Usage: java PGPEncryption <encryptDirectory> <recipient>");
+            return;
+        }
+        
         try{
             PGPEncryption pgpEncryption = new PGPEncryption(args[0], args[1], "afp");           
             pgpEncryption.encrypt();
